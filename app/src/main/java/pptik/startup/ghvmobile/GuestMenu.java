8b6b833bb6262efae310b3c4fea7ba0513a23224
achmad.ridho91@gmail.com <achmad.ridho91@gmail.com>
2016-08-06 11:52:25 +0700

    add map guestmenu

--- app/src/main/java/pptik/startup/ghvmobile/GuestMenu.java
+++ app/src/main/java/pptik/startup/ghvmobile/GuestMenu.java
@@ -7,14 +7,16 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -24,11 +26,23 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.ionicons_typeface_library.Ionicons;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
 
 import java.util.ArrayList;
 import java.util.Timer;
@@ -37,14 +51,16 @@ import java.util.TimerTask;
 import pptik.startup.ghvmobile.Support.CustomAdapter;
 import pptik.startup.ghvmobile.Support.ProfileRelawan;
 import pptik.startup.ghvmobile.Support.Program;
+import pptik.startup.ghvmobile.Utilities.PictureFormatTransform;
 import pptik.startup.ghvmobile.setup.ApplicationConstants;
 import pptik.startup.ghvmobile.Utilities.DrawerUtil;
-import pptik.startup.ghvmobile.Utilities.PictureFormatTransform;
+
 /**
  * Created by GIGABYTE on 27/06/2016.
  */
 
-public class GuestMenu extends AppCompatActivity {
+public class GuestMenu extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
     public int currentimageindex=0;
     //    Timer timer;
 //    TimerTask task;
@@ -67,54 +83,59 @@ public class GuestMenu extends AppCompatActivity {
     SharedPreferences prefs;
     public boolean status;
 
+    //keperluan map
+    private FloatingActionButton fabMyLoc, fabAdd;
+    private boolean isFirstZoom = false;
+    int permissionCheck =0;
+    private static final int INITIAL_REQUEST=1337;
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION=INITIAL_REQUEST+1;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private Location mLocation;
+    private String TAG = this.getClass().getSimpleName();
+    private double currentLatitude;
+    private double currentLongitude;
+    private Context context;
+    MapView mapset;
+    GeoPoint currentPoint;
+    Marker curMarker;
+    IMapController mapController;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.guest_activity);
+        setContentView(R.layout.guest_menu_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         applicationContext = this.getApplicationContext();
-
-        bt=(TextView) findViewById(R.id.btn_jadi_relawan) ;
-        bt2=(TextView) findViewById(R.id.btn_news_program_issue) ;
+        context = this;
+        permissionCheck = ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION);
+        initMap();
+        setLocationBuilder();
+        bindingXml();
         prefs = getSharedPreferences(ApplicationConstants.USER_PREFS_NAME,
                 Context.MODE_PRIVATE);
         String registrationId = prefs.getString(ApplicationConstants.REG_ID, "");
         String theRole = prefs.getString(ApplicationConstants.LEVEL_ID, "");
 
-       // checkstatusdaftar();
-        bt2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Guest.class);
-                startActivity(intent);
 
-            }
-        });
         final Handler mHandler = new Handler();
-
         // Create runnable for posting
         final Runnable mUpdateResults = new Runnable() {
             public void run() {
-
                 AnimateandSlideShow();
-
             }
         };
         int delay = 1000; // delay for 1 sec.
-
         int period = 15000; // repeat every 4 sec.
-
         Timer timer = new Timer();
-
         timer.scheduleAtFixedRate(new TimerTask() {
 
             public void run() {
-
                 mHandler.post(mUpdateResults);
-
             }
 
         }, delay, period);
+
         new DrawerUtil(this, toolbar, 0).initDrawerGuest();
     }
 
@@ -214,33 +235,165 @@ public class GuestMenu extends AppCompatActivity {
                     }
                 });
     }
-  /*  @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_index, menu);
-        return true;
+    private void setLocationBuilder(){
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)
+                .setFastestInterval(1 * 1000);
+    }
+    private void bindingXml(){
+        fabMyLoc = (FloatingActionButton)findViewById(R.id.fab_myloc_guest);
+        fabAdd = (FloatingActionButton)findViewById(R.id.fab_add_guest);
+        fabMyLoc.setImageBitmap(PictureFormatTransform.drawableToBitmap(new IconicsDrawable(this)
+                .icon(Ionicons.Icon.ion_android_locate)
+                .color(context.getResources().getColor(R.color.colorPrimary))
+                .sizeDp(60)));
+        fabAdd.setImageBitmap(PictureFormatTransform.drawableToBitmap(new IconicsDrawable(this)
+                .icon(Ionicons.Icon.ion_android_add)
+                .color(context.getResources().getColor(R.color.actlighorange))
+                .sizeDp(60)));
+
+        //--- Click Listeners
+        fabMyLoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                zoomMapToCurrent();
+            }
+        });
+    }
+    private void initMap() {
+        mapset = (MapView) findViewById(R.id.mainMap_guest);
+        mapset.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
+        mapset.setMultiTouchControls(true);
+        mapController = mapset.getController();
+
+        curMarker = new Marker(mapset);
+        curMarker.setTitle("My Location");
+        curMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        curMarker.setIcon(new IconicsDrawable(this)
+                .icon(Ionicons.Icon.ion_android_pin)
+                .color(context.getResources().getColor(R.color.colorPrimary))
+                .sizeDp(48));
+
+    }
+
+    private void zoomMapToCurrent(){
+        mapController.setZoom(25);
+        mapController.animateTo(currentPoint);
+        //  mapController.setCenter(currentPoint);
+        mapset.invalidate();
+        curMarker.setPosition(currentPoint);
+        mapset.getOverlays().add(curMarker);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Intent intent;
-        switch (item.getItemId()){
-            case R.id.logout:
-                getSharedPreferences("UserDetails",
-                        Context.MODE_PRIVATE).edit().clear().commit();
-                intent = new Intent(applicationContext, Login.class);
-                startActivity(intent);
-                finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+    public void onConnected(Bundle bundle) {
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
 
-    }*/
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+            // MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+
+        }
+        mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLocation != null) {
+            Toast.makeText(this, "Location Detected "+mLocation.getLatitude()+" "+
+                    mLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+            currentLatitude = mLocation.getLatitude();
+            currentLongitude = mLocation.getLongitude();
+            currentPoint = new GeoPoint(currentLatitude, currentLongitude);
+            zoomMapToCurrent();
+            isFirstZoom = true;
+        } else {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+            Toast.makeText(this, "Location not Detected", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
-    protected void onDestroy() {
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the functionality that depends on this permission.
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Connection Suspended");
+        mGoogleApiClient.connect();
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(TAG, "Connection failed. Error: " + connectionResult.getErrorCode());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        currentLatitude = location.getLatitude();
+        currentLongitude = location.getLongitude();
+        currentPoint = new GeoPoint(currentLatitude, currentLongitude);
+        if(isFirstZoom == false){
+            zoomMapToCurrent();
+            isFirstZoom = true;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //Now lets connect to the API
+        if(mGoogleApiClient.isConnected() == false)
+            mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected  void onDestroy(){
         super.onDestroy();
+        if(mGoogleApiClient.isConnected()){
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+        //
     }
     @Override
     public void onBackPressed() {
